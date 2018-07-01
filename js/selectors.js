f4aa8529ccbca0ea1c013ca0901884059db1ba84
Rachel Dodell <rachel@racheldodell.com>
2018-07-01 01:42:20 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
 // declare your functions here...
+function paragraphSelector() {
+  $(p);
+}
+
+function lastImageSelector() {
+  'img:last';
+}
++function ninjaBabySelector() {
++  $('#baby-ninja');
++}
++function divSelector() {
++  $('div');
++}
++function firstListItem() {
++  $('child-tag:first-child');
++} 
